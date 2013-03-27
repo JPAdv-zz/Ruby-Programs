8bf459cd31544336126e16d389d864265a0af9e1
Jose Padilla <Yunny@Obscura.local>
2013-03-27 01:58:44 -0700

    Finished Prob3

--- Simple.rb
+++
@@ -1,8 +0,0 @@
-#A really simple ruby program that takes
-#arguments and prints them out with a "Hello, [arg]".
-#
-#Author: Jose Padilla
-
-args = ARGF.argv
-args.each {|names| print "Hello, #{names}!\n"}
-print "Welcome to the World of Ruby!\n"
